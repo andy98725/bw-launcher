26b5ebf27c426eeb215e797dfc87b9ffa965b69a
Andy Hudson <andyhudson725@gmail.com>
2021-03-30 16:09:23 -0500

    Final Header

--- src/appManager.js
+++ src/appManager.js
@@ -1,4 +1,3 @@
-
 $ = require('jquery');
 $(function(){
 	clickPlay();
@@ -7,9 +6,17 @@ $(function(){
 
 function clickPlay(){
 	$(".main").load("play.html");
+
+	$("#play-btn").addClass("active");
+	$("#patch-btn").removeClass("active");
+	$("#set-btn").removeClass("active");
 }
 function clickSettings(){
 	$(".main").load("settings.html");
+
+	$("#play-btn").removeClass("active");
+	$("#set-btn").addClass("active");
+	$("#patch-btn").removeClass("active");
 }
 
 let patchHTML = null;
@@ -22,5 +29,9 @@ function clickPatch(){
 		$(".main").html(patchHTML);
 	else
 		$(".main").text("Patch Notes not found. Please check your internet connection.");
+
+		$("#play-btn").removeClass("active");
+		$("#set-btn").removeClass("active");
+		$("#patch-btn").addClass("active");
 }
 
--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences:{
-	    nodeIntegration: true
+    webPreferences: {
+      nodeIntegration: true
     }
   });
 
